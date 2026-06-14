5508c3a117f62a702a40e4db07d212f6ab0e29cb
himohitj <himohitj@gmail.com>
2026-06-15 02:27:37 +0530

    setup done

--- src/main/java/com/mohit/entity/Student.java
+++ src/main/java/com/mohit/entity/Student.java
@@ -1,4 +1,37 @@
 package com.mohit.entity;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "students")
 public class Student {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    @Column(name = "email")
+    private String email;
+
+    // Constructors
+    public Student() {}
+
+    public Student(String firstName, String lastName, String email) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+    }
+
+    // Getters & Setters (or use Lombok @Data)
+    // toString() for printing
+    @Override
+    public String toString() {
+        return "Student{id=" + id + ", name=" + firstName + " " + lastName + "}";
+    }
 }
--- src/main/java/com/mohit/util/HibernateUtil.java
+++ src/main/java/com/mohit/util/HibernateUtil.java
@@ -1,4 +1,28 @@
 package com.mohit.util;
 
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
 public class HibernateUtil {
+
+    private static SessionFactory sessionFactory;
+
+    static {
+        try {
+            // Reads hibernate.cfg.xml from classpath automatically
+            sessionFactory = new Configuration()
+                    .configure()
+                    .buildSessionFactory();
+        } catch (Exception e) {
+            throw new ExceptionInInitializerError("SessionFactory creation failed: " + e);
+        }
+    }
+
+    public static SessionFactory getSessionFactory() {
+        return sessionFactory;
+    }
+
+    public static void shutdown() {
+        getSessionFactory().close();
+    }
 }
